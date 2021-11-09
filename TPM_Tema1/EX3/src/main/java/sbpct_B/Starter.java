8847b3e93660692e672803608412011d86dbe2f8
Zamfir Adrian-Iulian <75637465+ShadowySnake@users.noreply.github.com>
2021-11-09 12:11:41 +0200

    Small update.
    Re-pushed the source code for EX3

--- TPM_Tema1/EX3/src/main/java/sbpct_B/Starter.java
+++ TPM_Tema1/EX3/src/main/java/sbpct_B/Starter.java
@@ -3,7 +3,7 @@ package sbpct_B;
 public class Starter {
     public static void main(String[] args) throws InterruptedException {
         try {
-            Manager manager = new Manager(3, 500, 100);
+            Manager manager = new Manager(3, 500, 3);
             manager.start();
         } catch (InterruptedException e) {
             e.printStackTrace();
